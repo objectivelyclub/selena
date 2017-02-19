58fbef7c9b1574f8a0249672ac563af8c7fd61b2
Cindy Xiao <cndxiao@gmail.com>
2017-02-18 23:38:57 -0700

    Make messages per qr code an argument.

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import mido
 import argparse
 argparser = argparse.ArgumentParser() 
 argparser.add_argument('input_path', help="The path of the input MIDI file.")
-argparser.add_argument('output_path', help="The path of the QR image file to output.")
+argparser.add_argument('output_path', help="The base pathname of the QR image files to output.")
+argparser.add_argument('--messages_per_qr', help="The number of MIDI messages a single QR code should contain.", default=26)
 args = argparser.parse_args()
 
 midiFile = mido.MidiFile(args.input_path)
 noteMessageList = midiparser.parseForNoteMessages(midiFile)
 noteByteList = midiparser.noteMessagesToBytes(noteMessageList, midiFile.ticks_per_beat)
 
-messagesPerQR = 26 
-for i in range(len(noteByteList) // messagesPerQR):
-    data = b''.join(noteByteList[i:i+messagesPerQR])
+for i in range(len(noteByteList) // args.messages_per_qr):
+    data = b''.join(noteByteList[i:i+args.messages_per_qr])
     image = qr.generateQRImage(data)
 
     image.save(args.output_path + str(i) + '.png')

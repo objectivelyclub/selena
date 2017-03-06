f27bed84365dc34b77ee97899760a282ea999ef4
Cindy Xiao <cndxiao@gmail.com>
2017-03-06 13:47:43 -0700

    Add and prettify Rijesh's code, which adds a QR header including a unique song number, an average number of messages per frame, and a header of program change data. Make track combining better. Also fix some QR codes being out of frame.

--- main.py
+++ main.py
@@ -3,24 +3,37 @@ import qr
 
 import mido
 import base64
+from random import randint
 
 import argparse
 argparser = argparse.ArgumentParser() 
 argparser.add_argument('input_path', help="The path of the input MIDI file.")
 argparser.add_argument('output_path', help="The base pathname of the QR image files to output.")
-argparser.add_argument('--messages_per_qr', help="The number of MIDI messages a single QR code should contain.", default=26)
+argparser.add_argument('--messages_per_qr', help="The number of MIDI messages a single QR code should contain.", default=50)
+argparser.add_argument('--frame_duration', help="The duration, in ms, of each frame of the generated GIF.", default=500)
 args = argparser.parse_args()
 
 midiFile = mido.MidiFile(args.input_path)
 noteMessageList = midiparser.parseForNoteMessages(midiFile)
+programHeader = midiparser.getMIDIProgramInfo(midiFile)
+noteByteList = midiparser.noteMessagesToBytes(noteMessageList)
+
+with open('raw_bytes_dump', 'wb') as fout:
+    for noteBytes in noteByteList:
+        fout.write(noteBytes)
+        fout.write(bytes([255]))
+
+songnum = randint(0, 255)
+msgsperhalfsecond = midiparser.getDataRate(midiFile)
 
 image_frames = []
-for i in range(len(noteByteList) // args.messages_per_qr):
-    # Add frame number at beginning of data
-    data = i.to_bytes(2, 'little') + b''.join(noteByteList[i:i+args.messages_per_qr])
+
+for i in range(len(noteByteList) // msgsperhalfsecond):
+    dataInd = i * msgsperhalfsecond
+    data = i.to_bytes(2, 'little') + songnum.to_bytes(1,'little') + programHeader + msgsperhalfsecond.to_bytes(1,'little') + b''.join(noteByteList[dataInd:dataInd+msgsperhalfsecond])
     data = base64.b64encode(data)
 
     image = qr.generateQRImage(data)
     image_frames.append(image)
 
-image_frames[0].save(args.output_path + ".gif", format='GIF', duration=1000, save_all=True, append_images=image_frames[1:], optimize=True)
+image_frames[0].save(args.output_path + ".gif", format='GIF', duration=args.frame_duration, save_all=True, append_images=image_frames[1:], optimize=True)
--- midiparser.py
+++ midiparser.py
@@ -1,35 +1,79 @@
 import mido
 import struct
+import time
 
 def parseForNoteMessages(midiFile):
     """
-    Parses the midi file for messages related to notes only.
-    Ignores all system real-time messages.
+    Parses the midi file for Channel Voice Messages,
+    i.e. all messages that have to do with note output.
     Args:
         midiFile: A mido.MidiFile object.
     Returns:
         A list of all note-related MIDI messages as mido.Message objects.
     """
     noteMessageList = []
-    single_track = mido.merge_tracks(midiFile.tracks)
 
-    for message in single_track:
-        if message.type == 'note_on' or message.type == 'note_off':
-            noteMessageList.append(message)
+    for message in midiFile: # This provides delta time in seconds, not ticks.
+        if not isinstance(message, mido.MetaMessage):
+                if message.type == 'note_on' or message.type == 'note_off':
+                    noteMessageList.append(message)
 
     return noteMessageList
 
-def noteMessagesToBytes(messageList, ticksPerBeat, tempo=500000):
+def getMIDIProgramInfo(midiFile):
+    """
+    Gets the Program (i.e. instrument) associated with each Channel.
+    Args:
+        midiFile: A mido.MidiFile object.
+        This file should only contain 1 track.
+    Returns:
+        A 16-byte array containing the Program Number for each channel.
+    """
+    ar = [0] * 16
+    for track in midiFile.tracks:
+        for message in track:
+            if message.type == 'program_change':
+                ar[message.channel] = message.program
+
+    packed_bytes = struct.pack('16B', *ar)
+    return packed_bytes
+
+def getDataRate(midiFile):
+    """
+    Generates a count of how many bytes, on average,
+    there are in half a second of the provided MIDI file.
+    """
+    n = 0
+    for message in midiFile:
+        n=n+1
+
+    print("Total Messages: " + str(n))
+    print("Total song length: " + str(midiFile.length))
+    
+    msgps = n/ midiFile.length
+
+    print("Messages per second: " + str(msgps))
+    bps = msgps*7
+    print("Bytes per second: " + str(bps))
+
+    print("Bytes per 1/2 second: " + str(bps/2))
+        
+    
+    return int(msgps/2) + 1 # +30
+    
+
+def noteMessagesToBytes(messageList):
     """
     Takes a list of note messages, and packs each note message into bytes.
+    Note messages are assumed to have their delta times in seconds,
+    rather than ticks.
+    Args:
+        messageList: A list of mido.MidiMessage objects.
     """
-    seconds_per_beat = tempo / 1000000.0
-    seconds_per_tick = seconds_per_beat / float(ticksPerBeat)
-
     byteList = []
     for message in messageList:
-        message_time = (int)(message.time * seconds_per_tick * 1000)
-        packed_bytes = struct.pack('IBBB', message_time, *message.bytes())
+        ls = message.bytes()
+        packed_bytes = struct.pack('I%dB' % (len(ls),), (int) (message.time*1000), *ls)
         byteList.append(packed_bytes)
 
     return byteList

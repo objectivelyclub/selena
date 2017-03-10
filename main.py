5f4ef943716cc3e6b3eaba3dd300246b073e92ae
Cindy Xiao <cndxiao@gmail.com>
2017-03-09 23:55:42 -0700

    Process multiple midi files, add more samples.

--- main.py
+++ main.py
@@ -7,34 +7,37 @@ from random import randint
 
 import argparse
 argparser = argparse.ArgumentParser() 
-argparser.add_argument('input_path', help="The path of the input MIDI file.")
-argparser.add_argument('output_path', help="The base pathname of the QR image files to output.")
+argparser.add_argument('input_path', nargs='+', help="The path of the input MIDI file.")
 argparser.add_argument('--messages_per_qr', help="The number of MIDI messages a single QR code should contain.", default=50)
 argparser.add_argument('--frame_duration', help="The duration, in ms, of each frame of the generated GIF.", default=700)
 argparser.add_argument('--debug_note_bytes', action='store_true', help="Whether to output a file, raw_bytes_dump, that contains all note messages in byte format.")
 args = argparser.parse_args()
 
-midiFile = mido.MidiFile(args.input_path)
-noteMessageList = midiparser.parseForNoteMessages(midiFile)
-programHeader = midiparser.getMIDIProgramInfo(midiFile)
-noteByteList = midiparser.noteMessagesToBytes(noteMessageList)
+for fname in args.input_path:
+    print(fname)
 
-if args.debug_note_bytes:
-    with open('raw_bytes_dump', 'wb') as fout:
-        for noteBytes in noteByteList:
-            fout.write(noteBytes)
+for fname in args.input_path:
+    midiFile = mido.MidiFile(fname)
+    noteMessageList = midiparser.parseForNoteMessages(midiFile)
+    programHeader = midiparser.getMIDIProgramInfo(midiFile)
+    noteByteList = midiparser.noteMessagesToBytes(noteMessageList)
 
-songnum = randint(0, 255)
-msgsperhalfsecond = midiparser.getDataRate(midiFile)
+    if args.debug_note_bytes:
+        with open('raw_bytes_dump', 'wb') as fout:
+            for noteBytes in noteByteList:
+                fout.write(noteBytes)
 
-image_frames = []
+    songnum = randint(0, 255)
+    msgsperhalfsecond = midiparser.getDataRate(midiFile)
 
-for i in range(len(noteByteList) // msgsperhalfsecond):
-    dataInd = i * msgsperhalfsecond
-    data = i.to_bytes(2, 'little') + songnum.to_bytes(1,'little') + programHeader + msgsperhalfsecond.to_bytes(1,'little') + b''.join(noteByteList[dataInd:dataInd+msgsperhalfsecond])
-    data = base64.b64encode(data)
+    image_frames = []
 
-    image = qr.generateQRImage(data)
-    image_frames.append(image)
+    for i in range(len(noteByteList) // msgsperhalfsecond):
+        dataInd = i * msgsperhalfsecond
+        data = i.to_bytes(2, 'little') + songnum.to_bytes(1,'little') + programHeader + msgsperhalfsecond.to_bytes(1,'little') + b''.join(noteByteList[dataInd:dataInd+msgsperhalfsecond])
+        data = base64.b64encode(data)
 
-image_frames[0].save(args.output_path + ".gif", format='GIF', duration=args.frame_duration, save_all=True, append_images=image_frames[1:], optimize=True)
+        image = qr.generateQRImage(data)
+        image_frames.append(image)
+
+    image_frames[0].save(fname + ".gif", format='GIF', duration=args.frame_duration, save_all=True, append_images=image_frames[1:], optimize=True)
--- qr.py
+++ qr.py
@@ -1,7 +1,7 @@
 import qrcode
 
 def generateQRImage(data):
-    qr = qrcode.QRCode(box_size=5, border=20)
+    qr = qrcode.QRCode(box_size=5, border=10)
     qr.add_data(data)
     qr.make(fit=True)
     image = qr.make_image()

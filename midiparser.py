e8fc3af5adf19f8b3547d784f517eef88292f52b
rijesh augustine <rijesh@ualberta.ca>
2017-03-16 16:03:30 -0600

    temp

--- midiparser.py
+++ midiparser.py
@@ -38,7 +38,7 @@ def getMIDIProgramInfo(midiFile):
     packed_bytes = struct.pack('16B', *ar)
     return packed_bytes
 
-def getDataRate(midiFile, duration):
+def getDataRate(midiFile, duration, minperiod = 500, maxperiod = 2000):
     """
     Given a MIDI file, returns a count of how many MIDI messages,
     on average, there are in a specified time duration.
@@ -46,20 +46,39 @@ def getDataRate(midiFile, duration):
         midiFile: A mido.midiFile object.
         duration: A duration of time, in milliseconds.
     """
+    l = 0
+    for message in midiFile:
+        l += 1
+
+    avgdataRate = l / midiFile.length
+
     duration = duration/1000.0
+    maxmsgsperframe = (int) avgdataRate * minperiod/1000
+    minmsgsperframe = (int) avgdataRate * maxperiod/1000
+
     n = 0
     msgtime = 0
     msgarr = []
+    timearr = []
     for message in midiFile:
         n += 1
         msgtime += message.time
-        if msgtime > duration :
+        if (n >= maxmsgsperframe or msgtime > duration and  :
+            timearr.append(msgtime)
             msgarr.append(n)
             msgtime = 0
             n = 0
 
     return msgarr
 
+def case1(n, maxmsgsperframe):
+    return (n >= maxmsgsperframe and msgtime > minperiod)
+
+def case2(n, maxmsgsperframe):
+    return (msgtime > duration)
+
+def case3(n, maxmsgsperframe):
+    return (n <= minmsgsperframe and msgtime < maxperiod)
 
 def noteMessagesToBytes(messageList):
     """
